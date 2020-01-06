af5b047c755ce424ea784914fd115d3b9599ce8f
Natalie Chin <natalie.chin@thoughtworks.com>
2020-01-06 11:34:45 +0800

    Run jar as a service

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,23 @@ apt-get update
 
 apt-get install default-jre -y
 
-rm -rf /vagrant/jar
-wget --no-check-certificate --content-disposition https://github.com/Thoughtworks-SEA-Capability/Infrastructure-101-Pathway/blob/master/week1/hello-spring-boot-0.1.0.jar?raw=true -P /vagrant/jar
-nohup java -jar /vagrant/jar/hello-spring-boot-0.1.0.jar &
+rm -rf /apps/jar
+wget --no-check-certificate --content-disposition https://github.com/Thoughtworks-SEA-Capability/Infrastructure-101-Pathway/blob/master/week1/hello-spring-boot-0.1.0.jar?raw=true -P /apps/jar
+
+groupadd -r appmgr
+useradd -r -s /bin/false -g appmgr jarapps
+
+echo "[Unit]
+Description=Run hello-spring-boot jar
+
+[Service]
+WorkingDirectory=/apps/jar
+ExecStart=/usr/bin/java -Xms128m -Xmx256m -jar hello-spring-boot-0.1.0.jar
+User=jarapps
+Type=simple
+Restart=on-failure
+RestartSec=10" > /etc/systemd/system/hello-spring-boot.service
+
+chown -R jarapps:appmgr /apps/jar
+systemctl daemon-reload
+systemctl start hello-spring-boot.service
